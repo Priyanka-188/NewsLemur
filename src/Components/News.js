0938d9e400bdf8951af2015f8a176b47a5951b1f
Priyanka-191925 <sainipriyanka1722001@gmail.com>
2023-05-16 22:31:19 +0530

    Refactoring lecture 33

--- src/Components/News.js
+++ src/Components/News.js
@@ -26,50 +26,35 @@ export class News extends Component {
     }; //setting the state in class based component
   }
 
-  async componentDidMount() {
-    let url =
+async updateNews(){
+  const url =
       // eslint-disable-next-line no-template-curly-in-string
-      `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=065d6f44936348a2a6ca7418f29d6bc5&page=1&pageSize=${this.props.pageSize}`;
+      `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=065d6f44936348a2a6ca7418f29d6bc5&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({loading:true});
     let data = await fetch(url);
     let parsedData = await data.json();
-    // console.log(parsedData);
+    console.log(parsedData);
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
       loading:false
     });
+
+}
+
+
+  async componentDidMount() {
+    this.updateNews()
   }
 
   handlePrevClick = async () => {
-    console.log("Previous");
-
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=065d6f44936348a2a6ca7418f29d6bc5&page=${this.state.page - 1}&pageSize=${ this.props.pageSize}`;
-    this.setState({loading:true});
-    let data = await fetch(url);
-    let parsedData = await data.json();
-  
-    this.setState({
-      page: this.state.page - 1,
-      articles: parsedData.articles,
-      loading:false
-    });
+      this.setState({page: this.state.page -1});
+    this.updateNews();
   };
 
   handleNextClick = async () => {
-    console.log("Next");
-    if (this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)) {
-    } else {
-      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=065d6f44936348a2a6ca7418f29d6bc5&page=${this.state.page + 1}&pageSize=${ this.props.pageSize}`;
-      this.setState({loading:true});
-      let data = await fetch(url);
-      let parsedData = await data.json();
-      this.setState({
-        page: this.state.page + 1,
-        articles: parsedData.articles,
-        loading:false
-      });
-    }
+    this.setState({page: this.state.page +1});
+    this.updateNews();
   };
 
   render() {
